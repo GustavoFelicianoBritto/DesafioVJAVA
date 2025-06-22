88fe5f6f967cc4eb13b429d4ad6ffd394cf18953
GustavoFelicianoBritto <gustavofelicianoemail@gmail.com>
2025-06-22 17:12:49 -0300

    Código basico area retangulo

--- src/DesafioVJAVA/Library.java
+++ src/DesafioVJAVA/Library.java
@@ -17,24 +17,29 @@ public class Library {
 		total = (dias *24)+horas;
 		
 		System.out.printf(horas<=0?"Você viajou por "+ dias +" dias, ou seja, "+ total +" horas.":"Você viajou por "+ dias +" dias e "+ horas+" horas, ou seja, "+ total +" horas.");
-
+		
 	}
 	
+	public static void AreaRetangulo()
+	{
+		//Receber base e altura de um retângulo e mostrar a área.
+		Scanner input = new Scanner(System.in);
+		double base,altura,area;
+		
+		System.out.println("Digite a base do retângulo: ");
+		base = input.nextDouble();
+		System.out.println("Digite a altura do retângulo: ");
+		altura = input.nextDouble();
+		
+		area = base*altura;
+		
+		System.out.printf("O retângulo de base %.1f metros e altura %.1f metros tem área de %.1f",base,altura,area);
+		
+		
+	}
+
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 	public static void Basic()
 	{
 		Scanner input = new Scanner(System.in);
@@ -42,16 +47,19 @@ public class Library {
 		
 		do
 		{
-		System.out.println("\n\nQual opção? \n0 - Sair\n1 - Horas viagem\n");
+		System.out.println("\n\nQual opção? \n0 - Sair\n1 - Horas viagem\n2 - Área Retângulo\n");
 		option=input.nextInt();
 		
 		switch (option)
 		{
 		case 0:
-			System.out.println("Voltando");
+			System.out.println("Voltando para menu principal");
 			break;
 		case 1:
 			HorasViagem();
+			break;
+		case 2:
+			AreaRetangulo();
 			break;
 		default:
 			System.out.println("Opção inválida");
--- src/DesafioVJAVA/main.java
+++ src/DesafioVJAVA/main.java
@@ -7,6 +7,8 @@ public class main {
 		Library.Basic();
 		
 		
+		
+		
 	}
 	
 	
